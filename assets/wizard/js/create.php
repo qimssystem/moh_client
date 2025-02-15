3e4991e218bef31a4eb5d1a0b17983ebb204052b
Ditlhake Shimah Matshube <ditlhakem@gmail.com>
2025-02-15 13:12:56 +0200

    html code

---
+++ assets/wizard/js/create.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "shimah";
+		// include Database connection file 
+		// include("../../config/db_config.php");
+		// $name = $_POST['add_facility_name'];
+		// $ftype = $_POST['add_facility_type'];
+		// $village = $_POST['add_facility_village'];
+		// $electrified = $_POST['add_powerconnection'];
+		// $power_type = $_POST['add_powerconnection_type'];
+		// $powerbackup_type = $_POST['add_power_backup'];
+			 // $lan = $_POST['add_lan_available'];
+		// $core_switch_con = $_POST['add_core_connection'];
+		// $gdn = $_POST['add_gdn_link'];
+		// $topology = $_POST['add_topology'];
+		// $router_type = $_POST['add_routertype'];
+		// $defaultgateway = $_POST['add_gateway'];
+		// $technology = $_POST['add_technology'];
+		// $speed = $_POST['add_speed'];
+		// $isp = $_POST['add_isp'];
+			 // $pc = 0;
+			 // $printers = 0;
+		
+		// $query = "INSERT INTO `facility`(`village`,`name`,`electrified`,`lan`,`gdn`,`defaultgateway`,`pc`,`printers`,`speed`,`technology`,`ftype`,`power_type`,`powerbackup_type`,`core_switch_con`,`topology`,`router_type`,`isp`) 
+		// VALUES ($village,'$name',$electrified,$lan,$gdn,'$defaultgateway',$pc,$printers,'$speed',$technology,$ftype,$power_type,$powerbackup_type,'$core_switch_con',$topology,$router_type,$isp)";
+        // if (!$result = mysqli_query($con, $query)) {
+			// echo  mysqli_error($con);
+			// exit(mysqli_error($con));
+		  // }
+       // echo "Recorded created successfully";
+?>
